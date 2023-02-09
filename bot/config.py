29e351dfee591d289b2ce5410efa600f840ae919
Hirusha21 <60113454+Hirusha21@users.noreply.github.com>
2023-02-09 15:11:27 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,23 +3,23 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("5662705097:AAERW7P1AJQvlJsW9vk8_0FM--LrB3iRKJs")
 
     SESSION_NAME = ":memory:"
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("2208116"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("e908365e42dcd3051c24d412102f356e")
 
     CLIENT_ID = os.environ.get("CLIENT_ID")
 
     CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("1339693473,vinukihansamali"))
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
-    AUTH_USERS = [BOT_OWNER, 754495556] + (
+    AUTH_USERS = [BOT_OWNER, 755681801] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
